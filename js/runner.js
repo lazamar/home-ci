f136071d6d3946d82a331e1713ce00d7ebb5f690
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-13 21:19:51 +0000

    Add timeout to runner.js

--- js/runner.js
+++ js/runner.js
@@ -10,34 +10,50 @@ var utils = require('./utils');
 module.exports = function runner(commandName, args, dir) {
   dir = dir || process.cwd();
 
-  console.log(dir);
   console.log(commandName + ' ' + [].join.call(args, ' '));
 
   //Change current working directory
   process.chdir(dir);
-
+  var timeoutTime = 60000; // 1min
   return new Promise(function (resolve, reject) {
-    //Say what command is being executed and where
-    var output = '$: ' + commandName + ' ' + args.join(' ') + '\n';
 
+    //Object to be returned
+    var res = { exitStatus: undefined, output: '' };
+
+    //Say what command is being executed and where
+    res.output = '$: ' + commandName + ' ' + args.join(' ') + '\n';
+
+    //Start execution
     var proc = spawn(commandName, args);
+
+    //Prepare process timeout in case it takes too long
+    var timeout = setTimeout(function () {
+      proc.kill('SIGINT');
+      res.output += utils.ansiColorise('red',
+          '\nTest timed out. Test duration over ' + timeoutTime);
+
+      res.exitStatus = 1;
+      resolve(res);
+    }, timeoutTime);
+
+    //Append data to output String
     proc.stdout.on('data', function (data) {
       var buff = new Buffer(data);
-      output += buff.toString('utf8');
+      res.output += buff.toString('utf8');
       console.log('>>>>>>>>>: ' + data);
     });
 
+    //Append error to output String
     proc.stderr.on('data', function (data) {
-      output += utils.ansiColorise('red', '\nERR: ' + data);
+      res.output += utils.ansiColorise('red', '\nERR: ' + data);
       console.log('>>ERROR>>: ' + data);
     });
 
+    //Return whatever we have
     proc.on('exit', function (code) {
-      var returnObj = {
-        exitStatus: code,
-        output: output,
-      };
-      resolve(returnObj);
+      clearTimeout(timeout);
+      res.exitStatus = code;
+      resolve(res);
     });
   });
 };
