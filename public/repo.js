ecb85b2e4d0a6f586da9bc75dabcc4eb29003ce0
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-21 22:18:16 +0000

    Add loading animation and status icon

---
+++ public/repo.js
@@ -0,0 +1,28 @@
+//This will be sent by the server
+var STATUS = {
+  success: true,
+
+  //Possible values: ['cloning', 'installing', 'pulling', 'testing', 'free'],
+  availability: 'installing',
+};
+
+(function (stats) {
+  'use strict';
+
+  var statusIcon = document.querySelector('.status-icon');
+  if (!statusIcon) {
+    console.error('No status icon found.');
+  } else if (stats.availability !== 'free') {
+    statusIcon.classList.add('fa-cog');
+    statusIcon.classList.add('warning-yellow');
+    var loadingBar = document.querySelector('.load-bar');
+    loadingBar.classList.remove('hidden');
+  } else if (stats.success) {
+    statusIcon.classList.add('fa-check-square');
+    statusIcon.classList.add('success-green');
+  } else {
+    statusIcon.classList.add('fa-minus-square');
+    statusIcon.classList.add('fail-red');
+  }
+
+}(STATUS));
