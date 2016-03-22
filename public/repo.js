83dd217d8bb6ea8f1008602f88693e1302af2a4d
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-22 00:11:37 +0000

    Add title to pages

--- public/repo.js
+++ public/repo.js
@@ -1,28 +1,30 @@
 //This will be sent by the server
 var STATUS = {
-  success: false,
+  success: true,
 
   //Possible values: ['cloning', 'installing', 'pulling', 'testing', 'free'],
-  availability: 'free',
+  availability: 'installing',
 };
 
 (function (stats) {
   'use strict';
 
   var statusIcon = document.querySelector('.status-icon');
-  if (!statusIcon) {
-    console.error('No status icon found.');
+  var dividingBar = document.querySelector('.dividing-bar');
+  if (!statusIcon || !dividingBar) {
+    console.error('No status icon or loading bar found.');
   } else if (stats.availability !== 'free') {
     statusIcon.classList.add('fa-cog');
+    statusIcon.classList.add('spinning');
     statusIcon.classList.add('warning-yellow');
-    var loadingBar = document.querySelector('.load-bar');
-    loadingBar.classList.remove('hidden');
   } else if (stats.success) {
     statusIcon.classList.add('fa-check-square');
     statusIcon.classList.add('success-green');
+    dividingBar.classList.add('success');
   } else {
     statusIcon.classList.add('fa-minus-square');
     statusIcon.classList.add('fail-red');
+    dividingBar.classList.add('failure');
   }
 
 }(STATUS));
