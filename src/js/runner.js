831f96b55c6d0bfb9099ec2333e5fde691a03caa
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-27 22:45:06 +0100

    Add Messager to Runner

--- src/js/runner.js
+++ src/js/runner.js
@@ -4,57 +4,105 @@
 
 var childProcess = require('child_process');
 var spawn = childProcess.spawn;
-var Promise = require('promise');
 var utils = require('./utils');
 
+function Messager() {
+  if (!(this instanceof Messager)) {
+    return new Messager();
+  }
+
+  var listeners = {};
+  var validEvents = ['message', 'complete'];
+  var output = '';
+
+  function isValidEvent(event) {
+    if (validEvents.indexOf(event) < 0) {
+      return false;
+    }
+    return true;
+  }
+
+  function on(event, callback) {
+    if (!isValidEvent(event)) {
+      throw new Error('runner Messsager(): ' + event + ' is not a valid Messager event.');
+    } else if (typeof callback !== 'function') {
+      throw new Error('runner Messsager(): The callback parameter is not a function.');
+    } else {
+      listeners[event] = callback;
+    }
+  }
+
+  function trigger(event) {
+    if (listeners[event]) {
+      var args = [].slice.call(arguments, 1);
+      listeners[event].apply(listeners[event], args);
+    }
+  }
+
+  function send(txt) {
+    // For now show in the console everything that is being sent
+    console.log(txt);
+
+    output += txt;
+    trigger('message', txt);
+  }
+
+  function complete(exitStatus) {
+    trigger('complete', output, exitStatus);
+  }
+
+  this.send = send;
+  this.on = on;
+  this.complete = complete;
+  // messager.on('message', function (msg) {});
+  // messager.on('complete', function (fullOutput, exitStatus) {});
+  return this;
+}
+
+
 module.exports = function runner(commandName, args, dir, timeoutTime) {
+  // Go to the directory where the command must be executed
   dir = dir || process.cwd();
-
-  console.log(commandName + ' ' + [].join.call(args, ' '));
-
-  //Change current working directory
   process.chdir(dir);
   timeoutTime = timeoutTime || 60000; // default: 1min
-  return new Promise(function (resolve, reject) {
 
-    //Object to be returned
-    var res = { exitStatus: undefined, output: '' };
+  // Object to be returned
+  var exitStatus = { success: 0, error: 1 };
+  var messager = new Messager();
+  messager.send('$: ' + commandName + ' ' + args.join(' ') + '\n');
 
-    //Say what command is being executed and where
-    res.output = '$: ' + commandName + ' ' + args.join(' ') + '\n';
+  // Start execution
+  var proc = spawn(commandName, args);
 
-    //Start execution
-    var proc = spawn(commandName, args);
+  // Prepare process timeout in case it takes too long
+  var timeout = setTimeout(function () {
+    proc.kill('SIGINT');
+    var err = utils.ansiColorise('red',
+      '\nCommand "' + commandName + ' ' + args.join(' ') + '" timed out.' +
+      ' Duration over ' + timeoutTime);
 
-    //Prepare process timeout in case it takes too long
-    var timeout = setTimeout(function () {
-      proc.kill('SIGINT');
-      res.output += utils.ansiColorise('red',
-          '\nCommand "' + commandName + ' ' + args.join(' ') + '" timed out.' +
-          ' Duration over ' + timeoutTime);
+    messager.send(err);
+    messager.complete(exitStatus.error);
+  }, timeoutTime);
 
-      res.exitStatus = 1;
-      resolve(res);
-    }, timeoutTime);
-
-    //Append data to output String
-    proc.stdout.on('data', function (data) {
-      var buff = new Buffer(data);
-      res.output += buff.toString('utf8');
-      console.log('>>>>>>>>>: ' + data);
-    });
-
-    //Append error to output String
-    proc.stderr.on('data', function (data) {
-      res.output += utils.ansiColorise('red', '\nERR: ' + data);
-      console.log('>>ERROR>>: ' + data);
-    });
-
-    //Return whatever we have
-    proc.on('exit', function (code) {
-      clearTimeout(timeout);
-      res.exitStatus = code;
-      resolve(res);
-    });
+  // Append data to output String
+  proc.stdout.on('data', function (data) {
+    var buff = new Buffer(data);
+    var output = buff.toString('utf8');
+    messager.send(output);
   });
+
+  // Append error to output String
+  proc.stderr.on('data', function (data) {
+    var output = utils.ansiColorise('red', '\nERR: ' + data);
+    messager.send(output);
+  });
+
+  // Return whatever we have
+  proc.on('exit', function (code) {
+    clearTimeout(timeout);
+    messager.complete(code);
+  });
+
+  return messager;
 };
